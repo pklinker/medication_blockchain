e8002d0ddd3b9dd01cd3d20c936ea9aeb8cc2808
pklinker <pklinker@gmail.com>
2019-05-15 11:22:09 -0400

    Added peers to manufacturing and hospital.

--- medications-network/start.sh
+++ medications-network/start.sh
@@ -10,7 +10,7 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.bigpharma.com ca.shipstuff.com ca.health.org orderer.bigpharma.com peer0.manufacturing.bigpharma.com peer0.shipping.shipstuff.com peer0.hospital.health.org couchdb
+docker-compose -f docker-compose.yml up -d ca.bigpharma.com ca.shipstuff.com ca.health.org orderer.bigpharma.com peer0.manufacturing.bigpharma.com peer1.manufacturing.bigpharma.com peer0.shipping.shipstuff.com peer0.hospital.health.org peer1.hospital.health.org couchdb
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -23,6 +23,11 @@ docker exec -e "CORE_PEER_LOCALMSPID=ManufacturingMSP" -e "CORE_PEER_MSPCONFIGPA
 # Join peer0.manufacturing.bigpharma.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=ManufacturingMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@manufacturing.bigpharma.com/msp" peer0.manufacturing.bigpharma.com peer channel join -b distribution.block
 
+# fetch the channel for the manufacturing peer1 organization
+docker exec -e "CORE_PEER_LOCALMSPID=ManufacturingMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@manufacturing.bigpharma.com/msp" peer1.manufacturing.bigpharma.com peer channel fetch newest distribution.block -o orderer.bigpharma.com:7050 -c distribution 
+# Join peer1.manufacturing.bigpharma.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=ManufacturingMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@manufacturing.bigpharma.com/msp" peer1.manufacturing.bigpharma.com peer channel join -b distribution.block
+
 # fetch the channel for the shipping organization
 docker exec -e "CORE_PEER_LOCALMSPID=ShippingMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@shipping.shipstuff.com/msp" peer0.shipping.shipstuff.com peer channel fetch newest distribution.block -o orderer.bigpharma.com:7050 -c distribution 
 # Join peer0.shipping.shipstuff.com to the channel.
@@ -30,6 +35,10 @@ docker exec -e "CORE_PEER_LOCALMSPID=ShippingMSP" -e "CORE_PEER_MSPCONFIGPATH=/e
 
 # fetch the channel for the pharmacy peer0 organization
 docker exec -e "CORE_PEER_LOCALMSPID=HospitalMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@hospital.health.org/msp" peer0.hospital.health.org peer channel fetch newest distribution.block -o orderer.bigpharma.com:7050 -c distribution 
-# Join peer0.pharma1.health.org to the channel.
+# Join peer0.hospital.health.org to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=HospitalMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@hospital.health.org/msp" peer0.hospital.health.org peer channel join -b distribution.block
 
+# fetch the channel for the pharmacy peer1 organization
+docker exec -e "CORE_PEER_LOCALMSPID=HospitalMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@hospital.health.org/msp" peer1.hospital.health.org peer channel fetch newest distribution.block -o orderer.bigpharma.com:7050 -c distribution 
+# Join peer1.hospital.health.org to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=HospitalMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@hospital.health.org/msp" peer1.hospital.health.org peer channel join -b distribution.block
