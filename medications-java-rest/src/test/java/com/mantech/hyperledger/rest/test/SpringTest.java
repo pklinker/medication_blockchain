d1a1bcaf17746f815eb08d57875e069a882f7587
pklinker <pklinker@gmail.com>
2019-09-03 10:35:35 -0400

    Remove broken maven test build.

--- medications-java-rest/src/test/java/com/mantech/hyperledger/rest/test/SpringTest.java
+++ medications-java-rest/src/test/java/com/mantech/hyperledger/rest/test/SpringTest.java
@@ -1,20 +1,16 @@
 package com.mantech.hyperledger.rest.test;
 
-import com.mantech.hyperledger.rest.WebConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-        classes = {WebConfig.class},
-        loader = AnnotationConfigContextLoader.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SpringTest {
 
     @Test
     public void whenSpringContextIsInstantiated_thenNoExceptions(){
-        // When
     }
 }
